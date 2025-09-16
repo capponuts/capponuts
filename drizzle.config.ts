a6c6298381e852468f665f6b882e2e0def2d0c89
capponuts <cappk3v@gmail.com>
2025-09-16 22:10:22 +0200

    build(drizzle): migrate to dialect=postgresql config for drizzle-kit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,14 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
-export default {
+export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./drizzle",
-  driver: "pg",
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL as string,
+    url: process.env.DATABASE_URL as string,
   },
   verbose: true,
   strict: true,
-} satisfies Config;
+});
 
 
